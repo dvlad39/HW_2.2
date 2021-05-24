ade4fca683390984c096d044c0f601ad05cd753a
Vladyslav Shreder <84121420+dvlad39@users.noreply.github.com>
2021-05-24 17:47:59 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace VariablesDemo
 {
@@ -7,31 +7,18 @@ namespace VariablesDemo
         static void Main(string[] args)
         {
             Console.Write("About which language do you want to know information? 1 - English, 2 - Spanish, 3 - Ukrainian ");
-            string line = Console.ReadLine();
             byte languageValue;
 
-            if (byte.TryParse(line, out languageValue))
+            while (!byte.TryParse(Console.ReadLine(), out languageValue) || languageValue > 3 || languageValue <= 0)
             {
-                byte x = byte.Parse(line);
-                if (x <= 3 && x > 0)
-                {
-                    Console.WriteLine($"With 2,500 to 3,000 words, you can understand 90% of everyday {(Language)languageValue} conversations,");
-                    Console.WriteLine($"{(Language)languageValue} newspaper and magazine articles, and {(Language)languageValue} used in the workplace.");
-                    Console.WriteLine($"So it is essential to learn the right {(Language)languageValue} vocabulary words.");
-
-                }
-             //    else
-            //        Console.WriteLine("Please follow the instructions and try again");
-
+                   
+                Console.WriteLine("Please follow the instructions and try again");
+                continue;
+                
             }
-            //else
-
-            //{
-
-            //    Console.WriteLine("Please follow the instructions and try again");
-
-            //}
-
+                Console.WriteLine($"With 2,500 to 3,000 words, you can understand 90% of everyday {(Language)languageValue} conversations,");
+                Console.WriteLine($"{(Language)languageValue} newspaper and magazine articles, and {(Language)languageValue} used in the workplace.");
+                Console.WriteLine($"So it is essential to learn the right {(Language)languageValue} vocabulary words.");
         }
 
         public enum Language : byte
@@ -39,9 +26,7 @@ namespace VariablesDemo
             English = 1,
             Spanish = 2,
             Ukrainian = 3,
-
-
-
         }
     }
 }
+
